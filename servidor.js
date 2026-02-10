5b8c0dce73c9c04d20515e4628a4f888e3213f2a
Khwerty <86443865+Khwerty@users.noreply.github.com>
2026-02-10 01:01:23 -0300

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -5,9 +5,10 @@ const server = dgram.createSocket('udp4');
 server.on('message', (msg, rinfo) => {
     console.log(`Paquete de ${rinfo.address}:${rinfo.port}`);
     console.log(`Tamaño del paquete: ${msg.length} bytes`);
+    console.log(msg.toString('utf8'));
     console.log('-------------------------------');
 });
 
 server.bind(PORT, () => {
-    console.log(`Servidor UDP escuchando en puerto ${PORT}`);
+    console.log(`I NEED MORE THAN ${PORT} MARES`);
 });
