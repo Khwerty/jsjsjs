4dd09eae941f22302c0d5fa17fe7feca7c3fb7eb
Khwerty <86443865+Khwerty@users.noreply.github.com>
2026-02-17 01:42:28 -0300

    A

--- servidor.js
+++ servidor.js
@@ -2,36 +2,34 @@ const dgram = require('dgram');
 const PORT = 54222;
 const server = dgram.createSocket('udp4');
 
-function parse_buffer(){
-    let offset = 0;
+function parse_buffer() {
+	let offset = 0;
 
-    let len =  msg.readUInt8(offset);
-    offset += 1;
+	let len = msg.readUInt8(offset);
+	offset += 1;
 
-    let lobby_name =  msg.toString("utf8", offset, offset + len);
-    offset += len;
+	let lobby_name = msg.toString("utf8", offset, offset + len);
+	offset += len;
 
-    let id = msg.readUInt8(offset);
-    offset += 1;
+	let id = msg.readUInt8(offset);
+	offset += 1;
 
-    let players = msg.readUInt8(offset);
-    offset += 1;
+	let players = msg.readUInt8(offset);
+	offset += 1;
 
-    let max_players = msg.readUInt8(offset);
-    offset += 1;
+	let max_players = msg.readUInt8(offset);
+	offset += 1;
 
-    let public = msg.readUInt8(offset);
-    offset += 1;
-    
+	let public = msg.readUInt8(offset);
+	offset += 1;
+
 }
 
 
 server.on("message", (msg, rinfo) => {
-    
-
-    console.log(lobby_name+" #"+id+" ( "+players+" / "+max_players+" )");
+	console.log(lobby_name + " #" + id + " ( " + players + " / " + max_players + " )");
 });
 
 server.bind(PORT, () => {
-    console.log(`I NEED MORE THAN ${PORT} MARES`);
+	console.log(`I NEED MORE THAN ${PORT} MARES`);
 });
